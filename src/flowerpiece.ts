9f328f1b780552938224c38e895923a2109798f5
ray <lei91122@gmail.com>
2020-04-28 17:53:58 +0800

    donot export piecetree

--- src/flowerpiece.ts
+++ src/flowerpiece.ts
@@ -1,8 +1,7 @@
-import { PieceTree } from './pieceTree'
 import { PieceMeta, IPieceMeta } from './meta'
 import { Piece, Line } from './piece'
 import { Diff } from './diff'
 import { Operations } from './operations'
 import { Model, ModelConfig } from './model'
 
-export { PieceTree, Piece, PieceMeta, IPieceMeta, Diff, Line, Operations, Model, ModelConfig }
+export { Piece, PieceMeta, IPieceMeta, Diff, Line, Operations, Model, ModelConfig }
