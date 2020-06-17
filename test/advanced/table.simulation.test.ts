6ef45a2b549e58c54c7442ac04f4c11cd3f983ec
ray <lei91122@gmail.com>
2020-06-17 19:12:08 +0800

    add test

--- test/advanced/table.simulation.test.ts
+++ test/advanced/table.simulation.test.ts
@@ -22,22 +22,16 @@ it('Table Simulation', () => {
    */
   operations.insertText(0, 'a')
   operations.insertLineBreak(1, { type: 'tb-s' })
-  operations.insertLineBreak(2, { type: 'tb-e' })
 
-  operations.insertLineBreak(2, { type: 'tr' })
-  operations.insertLineBreak(2, { type: 'tr' })
+  operations.insertLineBreak(2, { type: 'tc', num: 1 })
+  operations.insertLineBreak(3, { type: 'tc', num: 2 })
+  operations.insertLineBreak(4, { type: 'tc', num: 3 })
 
-  operations.insertLineBreak(3, { type: 'tc', num: 1 })
-  operations.insertLineBreak(4, { type: 'tc', num: 2 })
-  operations.insertLineBreak(5, { type: 'tc', num: 3 })
+  operations.insertLineBreak(5, { type: 'tc', num: 4 })
+  operations.insertLineBreak(6, { type: 'tc', num: 5 })
+  operations.insertLineBreak(7, { type: 'tc', num: 6 })
 
-  operations.insertLineBreak(7, { type: 'tc', num: 4 })
-  operations.insertLineBreak(8, { type: 'tc', num: 5 })
-  operations.insertLineBreak(9, { type: 'tc', num: 6 })
-
-  const np = model.pieceTree.findByOffset(2)
-
-  // console.log(np)
+  operations.insertText(3, 'abc\ncde')
 
   console.log(queries.getLines())
   console.log(queries.getPieces())
